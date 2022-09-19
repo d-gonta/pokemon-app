75773b99d9bbd926287474b050da414593f3d69e
Yuto Ozeki <ozeki.10.1@gmail.com>
2022-09-20 00:36:38 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import {getAllPokemon} from './utils/pokemon.js';
+import {getPokemon} from './utils/pokemon.js';
+import Card from './components/Card/Card';
 
 function App() {
+
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  // ポケモンデータ取得状態変数(true|false)
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  
+  useEffect(() => {
+    const feachPokemonData = async () => {
+      // 全てのポケモンデータを取得
+      let res = await getAllPokemon(initialURL);
+
+      // 各ポケモンの詳細データを取得
+      // console.log(res.results);
+      loadPokemon(res.results);
+
+      setLoading(false);
+    };
+
+    feachPokemonData();
+  },[])
+
+  // 各ポケモンの詳細データを取得
+  const  loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+
+      data.map((pokemom) => {
+
+        // 各ポケモンの詳細をフェッチ
+        let pokemonRecord = getPokemon(pokemom.url);
+
+        return pokemonRecord;
+      })
+    );
+
+    setPokemonData(_pokemonData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {loading ? (
+          <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <div className='pokemonCardContainer'>
+            {
+              pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemom={pokemon} />
+              })
+            }
+          </div>
+        </>
+      )}
     </div>
   );
 }
