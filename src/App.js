0252ff98bea3dfd574b0b67331e903d1a361504b
Yuto Ozeki <ozeki.10.1@gmail.com>
2022-10-06 23:32:55 +0900

    各種コンポーネントとスタイルの追加

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {getAllPokemon} from './utils/pokemon.js';
 import {getPokemon} from './utils/pokemon.js';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
   // ポケモンデータ取得状態変数(true|false)
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextUrl] = useState("");
+  const [prevURL, setPrevUrl] = useState("");
   
   useEffect(() => {
     const feachPokemonData = async () => {
@@ -18,7 +22,11 @@ function App() {
       let res = await getAllPokemon(initialURL);
 
       // 各ポケモンの詳細データを取得
-      // console.log(res.results);
+      console.log(res.next);
+
+      // レスポンスから前後のページURLを取得して、設定する
+      setNextUrl(res.next);
+      setPrevUrl(res.previous);
       loadPokemon(res.results);
 
       setLoading(false);
@@ -28,14 +36,14 @@ function App() {
   },[])
 
   // 各ポケモンの詳細データを取得
-  const  loadPokemon = async (data) => {
+  const loadPokemon = async (data) => {
     let _pokemonData = await Promise.all(
 
       data.map((pokemom) => {
 
         // 各ポケモンの詳細をフェッチ
         let pokemonRecord = getPokemon(pokemom.url);
-
+        // console.log(pokemonRecord);
         return pokemonRecord;
       })
     );
@@ -43,23 +51,61 @@ function App() {
     setPokemonData(_pokemonData);
   };
 
-  return (
-    <div className="App">
+  const handlePrevPage = async () => {
 
-      {loading ? (
-          <h1>ロード中・・・</h1>
-      ) : (
-        <>
-          <div className='pokemonCardContainer'>
-            {
-              pokemonData.map((pokemon, i) => {
-                return <Card key={i} pokemom={pokemon} />
-              })
-            }
-          </div>
-        </>
-      )}
-    </div>
+    if (!prevURL)
+    {
+      return;
+    }
+
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+
+    setLoading(false);
+
+  };
+
+  const handleNextPage = async () => {
+ 
+    setLoading(true);
+ 
+    let data = await getAllPokemon(nextURL);
+    // console.log(data);
+    await loadPokemon(data.results);
+ 
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Navbar />
+        <div className="App">
+
+          {loading ? (
+              <h1>ロード中・・・</h1>
+          ) : (
+            <>
+              <div className='pokemonCardContainer'>
+                {
+                  pokemonData.map((pokemon, i) => {
+                    return <Card key={i} pokemon={pokemon} />
+                  })
+                }
+              </div>
+              <div className='btn'>
+                <button onClick={handlePrevPage}>前へ</button>
+                <button onClick={handleNextPage}>次へ</button>
+              </div>
+            </>
+          )}
+        </div>
+    </>
   );
 }
 
